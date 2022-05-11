dd68f02d2d02466779631ee9d00914f63ed45557
Gonzalo Arca <goarca@itba.edu.ar>
2022-05-11 00:02:18 -0300

    :sparkles: add energy printing + refactor prints to scientific notation

--- post_processing/radiation_particle.py
+++ post_processing/radiation_particle.py
@@ -1,45 +1,87 @@
 MATTER_FILE_NAME = "matter.txt"
 DYNAMIC_FILE_NAME = "dynamic.txt"
 STATIC_FILE_NAME = "static.txt"
+ENERGY_FILE_NAME = "energy.txt"
 
 PARTICLE_XYZ_MOVEMENT_FILE_NAME = "particle_movement.xyz"
 MATTER_XYZ_FILE_NAME = "matter.xyz"
 
 
+def parse_static_parameters():
+    static_parameters = {}
 
-def generate_particle_movement():
+    with open(STATIC_FILE_NAME, 'r') as f:
+        for line_number, line in enumerate(f):
+            if line_number == 0:
+                static_parameters["distance_between_particles"] = float(line.split()[
+                                                                        0])
+            elif line_number == 1:
+                static_parameters["particles_per_row"] = int(line.split()[0])
+            elif line_number == 2:
+                static_parameters["box_height"] = float(line.split()[0])
+            elif line_number == 3:
+                static_parameters["box_width"] = float(line.split()[0])
+            elif line_number == 4:
+                static_parameters["initial_height"] = float(line.split()[0])
+            elif line_number == 5:
+                static_parameters["initial_speed"] = float(line.split()[0])
+            elif line_number == 6:
+                static_parameters["mass"] = float(line.split()[0])
+            elif line_number == 7:
+                static_parameters["charge"] = float(line.split()[0])
+            elif line_number == 8:
+                static_parameters["step_size"] = float(line.split()[0])
+            elif line_number == 9:
+                static_parameters["save_frequency"] = float(line.split()[0])
 
-    particle_movement_output_file = open(PARTICLE_XYZ_MOVEMENT_FILE_NAME,'w')
+    return static_parameters
+
+
+def parse_simulation_output():
+
+    static_parameters = parse_static_parameters()
+
+    mass = static_parameters["mass"]
+
+    particle_movement_output_file = open(PARTICLE_XYZ_MOVEMENT_FILE_NAME, 'w')
+    energy_output_file = open(ENERGY_FILE_NAME, 'w')
 
     with open(DYNAMIC_FILE_NAME) as f:
-        for index, line in enumerate(f):
-            # x y vx vy
+        for line_number, line in enumerate(f):
+            # x y vx vy potential_energy
             line = line.split()
-            particle_movement_output_file.write(f"1\nCOMMENT\n{' '.join(line)} -1\n")
-    
+
+            particle_movement_output_file.write(
+                f"1\nCOMMENT\n{' '.join(line)} -1\n")
+
+            kinetic_energy = mass * (float(line[2]) ** 2 + float(line[3]) ** 2)
+            potential_energy = float(line[4])
+            energy_output_file.write(f"{kinetic_energy + potential_energy}\n")
+
     particle_movement_output_file.close()
 
 
-def generate_matter():
-  
-  static_data_file = open(STATIC_FILE_NAME,'r')
+def generate_matter_file():
 
-  number_of_particles = int(static_data_file.readlines()[1]) ** 2
+    static_data_file = open(STATIC_FILE_NAME, 'r')
 
-  matter_output_file = open(MATTER_XYZ_FILE_NAME,'w')
-  matter_output_file.write(f"{number_of_particles}\nCOMMENT\n")
+    number_of_particles = int(static_data_file.readlines()[1]) ** 2
 
-  with open(MATTER_FILE_NAME,'r') as f:
+    matter_output_file = open(MATTER_XYZ_FILE_NAME, 'w')
+    matter_output_file.write(f"{number_of_particles}\nCOMMENT\n")
 
-    for line in f:
-      matter_output_file.write(line)
+    with open(MATTER_FILE_NAME, 'r') as f:
 
-  matter_output_file.close()
+        for line in f:
+            matter_output_file.write(line)
+
+    matter_output_file.close()
+
 
 def main():
-  generate_matter()
-  generate_particle_movement()
-  
+    generate_matter_file()
+    parse_simulation_output()
+
 
 if __name__ == "__main__":
-  main()
+    main()
