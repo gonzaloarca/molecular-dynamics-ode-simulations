879fbc65a4ebbcf232127ea588966bd7b425a3ac
Francisco Quesada <fquesada@itba.edu.ar>
2022-05-11 21:33:40 -0300

    :sparkles: read from file and plot in separeted functions

--- benchmarks/quadratic_error_vs_dt.py
+++ benchmarks/quadratic_error_vs_dt.py
@@ -1,5 +1,5 @@
 import os
-    import time
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -32,9 +32,12 @@ def get_quadratic_error(method_results, analytic_result):
 
 
 def quadratic_error_vs_dt():
-    dts = np.logspace(-5, -1, 1000)
+    dts = np.logspace(-5, -1, 10)
     quadratic_errors = []
     startTime = time.perf_counter()
+
+    output_file = open('./output_files/ECM.csv', 'w')
+
     for dt in dts:
         cmd = f"java -DstepSize={dt} -classpath ./target/classes ar.edu.itba.ss.moleculardynamics.dampedharmonicoscillator.Simulation"
         print(cmd)
@@ -42,11 +45,31 @@ def quadratic_error_vs_dt():
         results = get_results()
         quadratic_errors.append(get_quadratic_error(results[1:], results[0]))
 
-    for quadratic_error in (np.array(quadratic_errors)).T:
-        plt.plot(dts, quadratic_error)
+    for dt, quadratic_error in zip(dts, quadratic_errors):
+        output_file.write(f"{dt} {' '.join(map(str,quadratic_error))}\n")
 
     end_time = time.perf_counter()
     print(f"Time: {end_time - startTime}")
+    output_file.close()
+
+    return dts, quadratic_errors
+
+
+def get_quadratic_error_from_file():
+
+    input_file = open('./output_files/ECM.csv', 'r')
+    dts, quadratic_errors = [], []
+    for line in input_file:
+        line_data = line.split()
+        dts.append(float(line_data[0]))
+        quadratic_errors.append(list(map(float, line_data[1:])))
+
+    return dts, quadratic_errors
+
+
+def plot_quadratic_error_vs_dt(dts, quadratic_errors):
+    for quadratic_error in (np.array(quadratic_errors)).T:
+        plt.plot(dts, quadratic_error)
     plt.legend(["Verlet", "Beeman", "Gear-Predictor"])
     plt.yscale('log')
     plt.xscale('log')
@@ -54,4 +77,6 @@ def quadratic_error_vs_dt():
 
 
 if __name__ == '__main__':
-    quadratic_error_vs_dt()
+    dts, quadratic_errors = quadratic_error_vs_dt()
+    # dts, quadratic_errors = get_quadratic_error_from_file()
+    plot_quadratic_error_vs_dt(dts, quadratic_errors)
